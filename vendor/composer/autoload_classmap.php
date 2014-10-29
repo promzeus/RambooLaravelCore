1075be20512d29d2b5028307c3dd46298ad7f693
Evgenyi Podluzhnyi <e.podluzhnyi@gmail.com>
2014-10-29 19:09:15 +0300

    clear autoload_classmap

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -17,30 +17,7 @@ return array(
     'Whoops\\Provider\\Zend\\RouteNotFoundStrategy' => $vendorDir . '/filp/whoops/src/deprecated/Zend/RouteNotFoundStrategy.php',
     
     // Models
-//     'User' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/User.php',
-	'AppHelper' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/AppHelper.php',
-
-    'Shop' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Shop.php',
-    'Partner' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Partner.php',
-    'Cashier' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Cashier.php',
     
-    'Workshift' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Workshift.php',
-    
-    'Category' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Category.php',
-    
-    'Attribute' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Attribute.php',
-    'AttributeValue' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/AttributeValue.php',
-    
-    'Brand' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Brand.php',
-    
-    'Country' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Country.php',
-    'Region' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Region.php',
-    'City' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/City.php',
-    
-    'Composition' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Composition.php',
-    'CompositionValues' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/CompositionValues.php',
-    'Material' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Material.php',
-    'Color' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Color.php',
-    'Size' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/Size.php',
-    'ReferenceSize' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/orm/ReferenceSize.php',
+    //'User' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/User.php',
+    'AppHelper' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/models/AppHelper.php',
 );
