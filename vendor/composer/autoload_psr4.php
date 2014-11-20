fc2f2a8cd8a74dbf50260d4f9d744c7d7b6f29d6
Evgenyi Podluzhnyi <e.podluzhnyi@gmail.com>
2014-11-20 18:24:27 -0500

    up autoload psr-4

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -5,7 +5,8 @@
 $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
-return array(
-    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
-    'Ramboo\\' => array($baseDir . '/../services'),
-);
+return [
+    'Monolog\\' => [$vendorDir . '/monolog/monolog/src/Monolog'],
+    'Ramboo\\' => [$baseDir . '/../services'],
+    'Cashbox\\' => [$baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app']
+];
