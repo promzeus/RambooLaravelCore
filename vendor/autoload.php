76b883ca98d25209b9ac25d947bc139e56bb3c89
Mihman <miklblitz@yandex.ru>
2015-04-24 13:05:06 +0300

    Fixed migrate rollback

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit5444d48822bdeaadbbbc64b6f0c8c599::getLoader();
+return ComposerAutoloaderInit6c53223f2fb921c51614126f7dda0b55::getLoader();
