313dcd3e3e9f7852f684626c6e958b41ba3cc42b
Vladimir Cherednichenko <che@mqd.me>
2016-11-07 23:38:07 +0200

    added string compiler

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -146,6 +146,9 @@ return array(
     'Whoops\\Module' => $vendorDir . '/filp/whoops/src/deprecated/Zend/Module.php',
     'Whoops\\Provider\\Zend\\ExceptionStrategy' => $vendorDir . '/filp/whoops/src/deprecated/Zend/ExceptionStrategy.php',
     'Whoops\\Provider\\Zend\\RouteNotFoundStrategy' => $vendorDir . '/filp/whoops/src/deprecated/Zend/RouteNotFoundStrategy.php',
+    
+    'Wpb\String_Blade_Compiler\ViewServiceProvider' => $vendorDir . '/stringbladecompiler/src/ViewServiceProvider.php',
+    
     'Workshift' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/database/migrations/2014_11_05_130120_workshift.php',
     'createStockAndStockworkers' => $baseDir . '/../cashbox.ramboo/cashbox.ramboo.ru/app/database/migrations/2015_04_03_130000_createStockAndStockworkers.php',
 );
